194927ea66ce45f8f9926280825cf7e69f1605a0
zaebee <zaebuntu@gmail.com>
2026-06-07 08:21:11 +0300

    revert: remove ResizeObserver wrapper that caused nodes to disappear

--- src/index.js
+++ src/index.js
@@ -4,33 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const OrigResizeObserver = window.ResizeObserver;
-window.ResizeObserver = class extends OrigResizeObserver {
-  constructor(callback) {
-    let timer;
-    super((entries, observer) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => callback(entries, observer), 0);
-    });
-  }
-};
-
-window.addEventListener('error', (e) => {
-  if (e.message?.includes('ResizeObserver loop')) {
-    e.stopImmediatePropagation();
-    e.preventDefault();
-    return false;
-  }
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
