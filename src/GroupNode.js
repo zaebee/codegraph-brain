7543ea2cfabcfbb9a9dc43086ed6a97c6c4a9bcc
zaebee <zaebuntu@gmail.com>
2026-06-07 09:22:34 +0300

    chore: cleanup old .js files, add dist/ to gitignore

--- src/GroupNode.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function GroupNode({ data }) {
-  return (
-    <div className="group-node">
-      <div className="group-node-label">{data.label}</div>
-    </div>
-  );
-}
