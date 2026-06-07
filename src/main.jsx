e58f468c9f20687152b4d1a0832ee75e286c4dda
zaebee <zaebuntu@gmail.com>
2026-06-07 09:54:09 +0300

    fix: wrap App in ReactFlowProvider for useReactFlow hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { ReactFlowProvider } from '@xyflow/react';
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App />
+    <ReactFlowProvider>
+      <App />
+    </ReactFlowProvider>
   </StrictMode>
 );
